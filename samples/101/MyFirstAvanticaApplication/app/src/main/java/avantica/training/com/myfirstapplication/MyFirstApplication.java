858b2dfe6924da868c5ff608a7122e765d6e8c95
Samuel Reque Zambrana <samuel.reque@ecisolutions.com>
2018-10-18 10:17:48 -0400

    playing with sharedpreference

--- samples/101/MyFirstAvanticaApplication/app/src/main/java/avantica/training/com/myfirstapplication/MyFirstApplication.java
+++ samples/101/MyFirstAvanticaApplication/app/src/main/java/avantica/training/com/myfirstapplication/MyFirstApplication.java
@@ -1,10 +1,13 @@
 package avantica.training.com.myfirstapplication;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 public class MyFirstApplication extends Application {
     private Person user = new Person("samuel", R.drawable.ic_launcher_background);
     private static MyFirstApplication instance;
+
+    SharedPreferences preferences;
 
     public static MyFirstApplication getInstance() {
         return instance;
@@ -14,13 +17,25 @@ public class MyFirstApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        preferences = this.getSharedPreferences(MyFirstApplication.class.getName(), MODE_PRIVATE);
+        preferences.getString("color", "RED");
     }
 
     public Person getUser() {
-        return user;
+        return new Person(
+            preferences.getString(Person.USER_NAME, user.Name),
+            preferences.getInt(Person.USER_PHOTO, user.photoResId));
     }
 
     public void setUser(Person user) {
+        saveUser(user);
         this.user = user;
+    }
+
+    private void saveUser(Person user) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(Person.USER_NAME, user.Name);
+        editor.putInt(Person.USER_PHOTO, user.photoResId);
+        editor.apply();
     }
 }
--- samples/101/MyFirstAvanticaApplication/app/src/main/java/avantica/training/com/myfirstapplication/Person.java
+++ samples/101/MyFirstAvanticaApplication/app/src/main/java/avantica/training/com/myfirstapplication/Person.java
@@ -3,6 +3,9 @@ package avantica.training.com.myfirstapplication;
 import android.content.Context;
 
 class Person {
+    public static final String USER_NAME="username";
+
+    public static final String USER_PHOTO="photo";
     public String Name;
     public int photoResId;
 
